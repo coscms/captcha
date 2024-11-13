2e8b3b66dea573cccb77e1375b89efa0e4268e2a
Hank Shen <swh@admpub.com>
2024-11-13 16:45:29 +0800

    update

--- driver.go
+++ driver.go
@@ -22,6 +22,36 @@ type Storer interface {
 	Delete(ctx context.Context, key string) error
 }
 
+type APIResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	*Data   `json:",omitempty"`
+}
+
+func (a *APIResponse) SetSuccess(message ...string) *APIResponse {
+	a.Code = 0
+	if len(message) > 0 {
+		a.Message = message[0]
+	}
+	return a
+}
+
+func (a *APIResponse) SetError(message string) *APIResponse {
+	a.Code = 1
+	a.Message = message
+	return a
+}
+
+func (a *APIResponse) SetData(data *Data) *APIResponse {
+	a.Code = 0
+	a.Data = data
+	return a
+}
+
+func (a *APIResponse) IsSuccess() bool {
+	return a.Code == 1
+}
+
 type Data struct {
 	Key   string `json:"key"`
 	Image string `json:"image,omitempty"`
--- example/main.go
+++ example/main.go
@@ -14,12 +14,6 @@ import (
 //go:embed native
 var static embed.FS
 
-type response struct {
-	Code          int    `json:"code"`
-	Message       string `json:"message"`
-	*captcha.Data `json:",omitempty"`
-}
-
 func main() {
 	store, err := cache.NewCacher(context.Background(), `memory`, cache.Options{Interval: int(captcha.MaxAge)})
 	if err != nil {
@@ -33,31 +27,33 @@ func main() {
 	mux.Handle(`/`, http.FileServer(http.FS(static)))
 	mux.HandleFunc(`GET /captcha/{driver}/{type}`, func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		resp := captcha.APIResponse{}
 		c, err := captcha.GetInstance(r.PathValue(`driver`), r.PathValue(`type`))
 		if err != nil {
-			captcha.JSON(w, response{Code: 1, Message: err.Error()})
+			captcha.JSON(w, resp.SetError(err.Error()))
 			return
 		}
 		data, err := c.MakeData(ctx)
 		if err != nil {
-			captcha.JSON(w, response{Code: 1, Message: err.Error()})
+			captcha.JSON(w, resp.SetError(err.Error()))
 			return
 		}
-		captcha.JSON(w, response{Code: 0, Data: data})
+		captcha.JSON(w, resp.SetData(data))
 	})
 	mux.HandleFunc(`POST /captcha/{driver}/{type}`, func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		resp := captcha.APIResponse{}
 		c, err := captcha.GetInstance(r.PathValue(`driver`), r.PathValue(`type`))
 		if err != nil {
-			captcha.JSON(w, response{Code: 1, Message: err.Error()})
+			captcha.JSON(w, resp.SetError(err.Error()))
 			return
 		}
 		err = c.Verify(ctx, r.FormValue(`key`), r.FormValue(`response`))
 		if err != nil {
-			captcha.JSON(w, response{Code: 1, Message: err.Error()})
+			captcha.JSON(w, resp.SetError(err.Error()))
 			return
 		}
-		captcha.JSON(w, response{Code: 0})
+		captcha.JSON(w, resp.SetSuccess())
 	})
 	println(`listen on http://127.0.0.1:4444`)
 	err = http.ListenAndServe(`:4444`, mux)
